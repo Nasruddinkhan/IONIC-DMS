d0313c7c387e0013ef30e5da7a90169924e665d2
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-06-01 20:04:58 +0530

    add cancel demands list

--- src/app/pages/demand/view-demand/cancel-view-demand/cancel-demands-list.page.ts
+++ src/app/pages/demand/view-demand/cancel-view-demand/cancel-demands-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../model/manual.request.model';
+import { ManualRequestService } from '../../../../service/manual-request/manual-request.service';
 
 
 @Component({
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cancel-demands-list.page.html'
 })
 export class CancelDemandsListPage implements OnInit {
+  employees: Employee[];
+  constructor(private manaulService: ManualRequestService) { }
   ngOnInit() {
-
+    this.employees = this.manaulService.getAllManualRequest();
   }
 }
